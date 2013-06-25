a9da6cab7bd6489d30e4ee2628bbfb49e49894a1
DanielMM <moisdaniel7@gmail.com>
2013-06-25 16:38:17 +0200

    Allow counselors to close/archive questions and coordinators to reopen them.

--- sites/all/themes/ungoko/templates/node--question.tpl.php
+++ sites/all/themes/ungoko/templates/node--question.tpl.php
@@ -21,11 +21,8 @@
     hide($content['comments']);
     hide($content['links']);
     hide($content['field_tags']);
-    print render($content);
+    print render($content['body']);
   ?>
-  <?php if(user_access('view question status')): ?> 
-    <a class="btn btn-primary">Archive</a>
-  <?php endif;?>
   
   <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
     <footer>
@@ -35,5 +32,27 @@
   <?php endif; ?>
 
   <?php print render($content['comments']); ?>
+
+  <?php
+  //Display the value of the status field if the user has the needed permission
+   if(user_access('view question status')): ?> 
+    <?php print render($content['field_status']['#items'][0]['value']); ?>
+  <?php endif;?>
+
+  <?php
+  //Display the button to allow users to archive questions
+   if(user_access('archive question') && $content['field_status']['#items'][0]['value'] == 'pending'): ?>
+    <?php  
+        print drupal_render(drupal_get_form('cfdp_uf_form'));
+    ?>
+  <?php endif;?>
+
+  <?php
+  // Display the button to allow users to reopen archived questions
+   if(user_access('reopen question') && $content['field_status']['#items'][0]['value'] == 'archived'): ?>
+    <?php  
+        print drupal_render(drupal_get_form('cfdp_uf_form'));
+    ?>
+  <?php endif;?>
 
 </article> <!-- /.node -->
