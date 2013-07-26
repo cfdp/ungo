feee506f6dba7007f60639a28c4a1970b3bfb261
DanielMM <moisdaniel7@gmail.com>
2013-07-26 15:42:16 +0200

    Add question status and archive/open btn to submitted section.

--- sites/all/themes/ungoko/templates/node--question.tpl.php
+++ sites/all/themes/ungoko/templates/node--question.tpl.php
@@ -19,6 +19,28 @@
   ?>
     <?php if ($display_submitted): ?>
       <div class="submitted">
+        <?php
+        //Display the value of the status field if the user has the needed permission
+         if(user_access('view question status')): ?> 
+          <div class="question-status pull-left">
+            Status: <?php print render($content['field_status']['#items'][0]['value']); ?>
+          </div>
+        <?php endif;?>
+
+        <?php
+        //Display the button to allow users to archive questions
+         if(user_access('archive question') && $content['field_status']['#items'][0]['value'] == 'pending'): ?>
+          <span class="pull-left"><?php print drupal_render(drupal_get_form('cfdp_uf_form')); ?></span>
+        <?php endif;?>
+
+        <?php
+        // Display the button to allow users to reopen archived questions
+         if(user_access('reopen question') && $content['field_status']['#items'][0]['value'] == 'archived'): ?>
+          <?php  
+              print drupal_render(drupal_get_form('cfdp_uf_form'));
+          ?>
+        <?php endif;?>
+
         <?php //print $user_picture; ?>
         <?php print $submitted; ?>
       </div>
@@ -32,27 +54,5 @@
   <?php endif; ?>
 
   <?php print render($content['comments']); ?>
-
-  <?php
-  //Display the value of the status field if the user has the needed permission
-   if(user_access('view question status')): ?> 
-    <?php print render($content['field_status']['#items'][0]['value']); ?>
-  <?php endif;?>
-
-  <?php
-  //Display the button to allow users to archive questions
-   if(user_access('archive question') && $content['field_status']['#items'][0]['value'] == 'pending'): ?>
-    <?php  
-        print drupal_render(drupal_get_form('cfdp_uf_form'));
-    ?>
-  <?php endif;?>
-
-  <?php
-  // Display the button to allow users to reopen archived questions
-   if(user_access('reopen question') && $content['field_status']['#items'][0]['value'] == 'archived'): ?>
-    <?php  
-        print drupal_render(drupal_get_form('cfdp_uf_form'));
-    ?>
-  <?php endif;?>
 
 </article> <!-- /.node -->
