62c9c5c64138344743548e0a38a3749b81721055
Benjamin Christensen <udvikling@greenvolcano.dk>
2018-10-09 15:18:47 +0200

    Installing Google Analytics as module

--- sites/all/themes/ungoko/templates/html.tpl.php
+++ sites/all/themes/ungoko/templates/html.tpl.php
@@ -17,16 +17,5 @@
   <?php print $page; ?>
   <?php print $page_bottom; ?>
 
-  <script>
-  // Google Analytics
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-45287818-1', 'ungokonomi.dk');
-  ga('send', 'pageview');
-
-</script>
 </body>
 </html>
